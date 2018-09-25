0be361526ddffc184ccf8bb0780c3eaa7a39cbd7
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-09-25 14:01:18 -0500

    Version 1.0.1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Routes from "./Routes";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
